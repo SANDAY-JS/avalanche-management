26187adf5ea39a10c89aa8535e4ad67954fed36b
SANDAY-JS <san.sh.s@i.softbank.jp>
2022-07-26 22:08:55 +0900

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import './app.css'
+
+export function App() {
+
+  return (
+    <>
+      <h1><span class='text-red-400 italic font-bold'>AVALANCHE</span> Team Management System</h1>
+    </>
+  )
+}
