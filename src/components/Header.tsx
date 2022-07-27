427cbc27b6940111178beb9040ddcbe28a857170
SANDAY-JS <san.sh.s@i.softbank.jp>
2022-07-27 20:55:53 +0900

    Adjusted bg-color when darkmode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
+import { useContext } from "preact/hooks"
+import { StateContext } from "../app"
+
 function Header() {
+  const context = useContext(StateContext)
+  
   return (
-    <div class="sticky top-0 flex items-center justify-center w-full py-3 shadow-md">
+    <div class={`sticky top-0 flex items-center justify-center w-full py-3 shadow-md ${context.dark && 'bg-[#1a1a1a]'}`}>
         <h1 class="text-lg"><span class='text-theme italic font-bold'>AVALANCHE</span> 練習管理</h1>
     </div>
   )
