257964aff19ce21c3b8e0ae7f539ec07e6dc46dc
SANDAY-JS <san.sh.s@i.softbank.jp>
2022-07-28 15:14:17 +0900

    Added Music Components

--- src/components/Music/SongList.tsx
+++ src/components/Music/SongList.tsx
@@ -1,18 +1,34 @@
+import Metronome from './Metronome';
+
 type Props = {
-    songs: Song[]
+  song: Song
+  id: number
+  currentSong: number | undefined
+  setCurrentSong: Function
 }
 
-function SongList({songs}: Props) {
+function Song({song, id, currentSong, setCurrentSong}: Props) {
+
+  const changeCurrentSong = () => {
+    if(currentSong !== id) return setCurrentSong(id);
+    setCurrentSong(undefined)
+  }
+
   return (
-    <div>
-        {songs.map((song, i) => {
-            <div key={i} class='flex items-center px-3 py-1' >
-                <p>Song {i+1}</p>
-                {/* <p>{song.name}</p> */}
-            </div>
-        })}
+    <div class='flex flex-col items-center gap-3 px-6 py-2 shadow-lg w-full rounded-lg'>
+      <h4 class="font-bold">{song.name}</h4>
+      <div className="flex items-center justify-between space-x-3 h-full w-full">
+        <div className="w-4/5">
+          <p><span class="font-semibold">{song.bpm}</span> bpm（{song.time_signature}）</p>
+          <p>{song.length} 分</p>
+        </div>
+        {/* <span class="h-12 w-[2px] bg-slate-400 mr-5 " /> */}
+        <div onClick={changeCurrentSong} class={`w-7 h-7 ${currentSong === id && 'fill-theme'}`}>
+          <Metronome />
+        </div>
+      </div>
     </div>
   )
 }
 
-export default SongList
+export default Song
