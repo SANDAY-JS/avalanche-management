08fb0697c663e42ff72d7dc5fc2a9421d501de87
SANDAY-JS <san.sh.s3@outlook.jp>
2022-08-06 16:32:25 +0900

    Added Apolo client & graphql

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,31 @@
 import { render } from 'preact'
 import { App } from './app'
 import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
-render(<App />, document.getElementById('app') as HTMLElement)
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result.data));
+
+render(
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>, 
+        document.getElementById('app') as HTMLElement
+    )
